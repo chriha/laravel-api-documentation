e40e0096c54a2665608dde443274f6ec1e160708
Christopher Hartmann <chriha84@gmail.com>
2021-05-05 15:08:27 +0200

    feat: cache API version info endpoint

--- src/Http/Controllers/ApiController.php
+++ src/Http/Controllers/ApiController.php
@@ -6,6 +6,7 @@ use Chriha\ApiDocumentation\Support\File;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Routing\Controller as BaseController;
 use Illuminate\Support\Arr;
+use Illuminate\Support\Facades\Cache;
 use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 use Symfony\Component\Yaml\Yaml;
 
@@ -14,24 +15,26 @@ class ApiController extends BaseController
 
     public function __construct()
     {
-        if ( ! empty(config('api-documentation.middleware.api'))) {
+        if (! empty(config('api-documentation.middleware.api'))) {
             $this->middleware(config('api-documentation.middleware.api'));
         }
     }
 
     public function index(string $version) : JsonResponse
     {
-        if ( ! File::exists($version)) {
-            throw new NotFoundHttpException;
-        }
+        return Cache::rememberForever("api.{$version}.index", function () use ($version) {
+            if (! File::exists($version)) {
+                throw new NotFoundHttpException;
+            }
 
-        $spec = Yaml::parseFile(File::path($version), Yaml::PARSE_OBJECT);
+            $spec = Yaml::parseFile(File::path($version), Yaml::PARSE_OBJECT);
 
-        foreach (config("api-documentation.specifications.hide.{$version}", []) as $key) {
-            Arr::forget($spec['info'], $key);
-        }
+            foreach (config("api-documentation.specifications.hide.{$version}", []) as $key) {
+                Arr::forget($spec['info'], $key);
+            }
 
-        return new JsonResponse($spec['info'] ?? ['version' => 'undefined']);
+            return new JsonResponse($spec['info'] ?? ['version' => 'undefined']);
+        });
     }
 
 }
